c0d347059db7793cd035e5fdb93e18dd0391fb9c
samuelnub <notsamnub@gmail.com>
2017-01-20 01:03:15 +0000

    yes

--- scripts/index.js
+++ scripts/index.js
@@ -7,17 +7,18 @@ window.onload = () => {
     });
 
     peer.on("signal", function (data) {
+        console.log("Take me down to new york city, where the yorks are new and the city is city");
         document.getElementById("yourId").value = JSON.stringify(data);
     });
 
     document.getElementById("connect").addEventListener("click", function () {
         var otherId = JSON.parse(document.getElementById("otherId").value);
-        peer.signal(otherId)
+        peer.signal(otherId);
     });
 
     document.getElementById("send").addEventListener("click", function () {
         var yourMessage = document.getElementById("yourMessage").value;
-        peer.send(yourMessage)
+        peer.send(yourMessage);
     });
 
     peer.on("data", function (data) {
