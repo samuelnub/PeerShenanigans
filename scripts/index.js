5a91110475bddf0a8d49a01d44d10db15e0bbb59
samuelnub <notsamnub@gmail.com>
2017-01-23 21:52:53 +0000

    y...es?

--- scripts/index.js
+++ scripts/index.js
@@ -3,10 +3,48 @@ console.log("hey!");
 window.onload = () => {
     var peer = new SimplePeer({
         initiator: location.hash === "#1",
+        config: {
+            iceServers: [
+                { url: 'stun:stun01.sipphone.com' },
+                { url: 'stun:stun.ekiga.net' },
+                { url: 'stun:stun.fwdnet.net' },
+                { url: 'stun:stun.ideasip.com' },
+                { url: 'stun:stun.iptel.org' },
+                { url: 'stun:stun.rixtelecom.se' },
+                { url: 'stun:stun.schlund.de' },
+                { url: 'stun:stun.l.google.com:19302' },
+                { url: 'stun:stun1.l.google.com:19302' },
+                { url: 'stun:stun2.l.google.com:19302' },
+                { url: 'stun:stun3.l.google.com:19302' },
+                { url: 'stun:stun4.l.google.com:19302' },
+                { url: 'stun:stunserver.org' },
+                { url: 'stun:stun.softjoys.com' },
+                { url: 'stun:stun.voiparound.com' },
+                { url: 'stun:stun.voipbuster.com' },
+                { url: 'stun:stun.voipstunt.com' },
+                { url: 'stun:stun.voxgratia.org' },
+                { url: 'stun:stun.xten.com' },
+                {
+                    url: 'turn:numb.viagenie.ca',
+                    credential: 'muazkh',
+                    username: 'webrtc@live.com'
+                },
+                {
+                    url: 'turn:192.158.29.39:3478?transport=udp',
+                    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                    username: '28224511:1379330808'
+                },
+                {
+                    url: 'turn:192.158.29.39:3478?transport=tcp',
+                    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                    username: '28224511:1379330808'
+                }
+            ]
+        },
         trickle: false
     });
 
-    if(SimplePeer.WEBRTC_SUPPORT) {
+    if (SimplePeer.WEBRTC_SUPPORT) {
         console.log("WebRTC's supported, so uh, that's a first step.");
     }
     else {
@@ -14,7 +52,7 @@ window.onload = () => {
         document.getElementById("messages").textContent += "Error: " + "WebRTC isn't supported on your browser/machine :(" + "\n";
     }
 
-    if(location.hash === "#1") {
+    if (location.hash === "#1") {
         document.getElementById("yourId").setAttribute("placeholder", "Wait a second...");
     }
 
@@ -31,7 +69,7 @@ window.onload = () => {
         console.log(peer);
     });
 
-    peer.on("connect", function() {
+    peer.on("connect", function () {
         document.getElementById("messages").textContent += "Status: " + "Your peer just successfully connected! Tickle me upside-down and call me bamboozled, it worked!" + "\n";
     });
 
@@ -46,7 +84,7 @@ window.onload = () => {
         document.getElementById("messages").textContent += "Them: " + data + "\n";
     });
 
-    peer.on("close", function() {
+    peer.on("close", function () {
         document.getElementById("messages").textContent += "Status: " + "Your peer just left you :( it's ok man." + "\n";
     });
 
